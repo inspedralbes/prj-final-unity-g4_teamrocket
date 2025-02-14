bcd0df794b9cc237fdf602c228453cc8a27a1b0f
a23matcasmar <a23matcasmar@inspedralbes.cat>
2025-02-14 02:02:21 +0100

    Implementar mecánica de recolectar objetos

--- Projecte Final/Assets/Scripts/TerminadaPartida.cs
+++ Projecte Final/Assets/Scripts/TerminadaPartida.cs
@@ -4,6 +4,11 @@ using UnityEngine.SceneManagement;
 public class TerminadaPartida : MonoBehaviour
 {
     public Collider2D miHitBox;
+    public GameObject item1;
+    public GameObject item2;
+    public GameObject item3;
+
+    private bool SalidaActive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +19,12 @@ public class TerminadaPartida : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(item1.activeSelf == false && item2.activeSelf == false && item3.activeSelf == false && SalidaActive)
+        {
+            Debug.Log("Items recolectados!");
+            GameObject.Find("HabilitarSalida").SetActive(false);
+            SalidaActive = false;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
