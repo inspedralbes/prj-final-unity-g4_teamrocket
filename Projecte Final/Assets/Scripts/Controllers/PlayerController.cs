10be91e742e04994e8a949404214779a7d5ba49c
a18marcastru <a18marcastru@inspedralbes.cat>
2025-04-11 12:10:42 +0200

    Barra de stamine.

--- Projecte Final/Assets/Scripts/Controllers/PlayerController.cs
+++ Projecte Final/Assets/Scripts/Controllers/PlayerController.cs
@@ -10,8 +10,13 @@ public class PlayerController : MonoBehaviour
     public float speedBase = 5f; // Velocidad normal del jugador
     public float speedBoost = 10f; // Velocidad duplicada cuando se presiona Shift
     public float maxHealth = 100f;
+    public int stamina = 100;
+    public int staminaMax = 100;
+    public int regeneracioStamina = 1;
 
     public HealthBar healthBar;
+    public BarraStamina barraStamina;
+    public float velocidadConsumoStamina = 0.1f;
     // private bool controlesInvertidos = false;
     // private float tiempoRestanteInversion = 0f;
 
@@ -19,6 +24,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         healthBar.SetMaxHealth(maxHealth);
+        barraStamina.SetMaxStamina(stamina);
     }
 
     void Update()
@@ -28,11 +34,34 @@ public class PlayerController : MonoBehaviour
         // TiempoEspejoRestante();
         // Si se mantiene presionada la tecla Shift, la velocidad se duplica
         speed = Input.GetKey(KeyCode.LeftShift) ? speedBoost : speedBase;
+
+        if (Speed == SpeedBoost) {
+            ReducirStamina(1);
+        }
+        else {
+            if (stamina < staminaMax){
+                stamina += regeneracioStamina;
+                barraStamina.ActualizarStamina(stamina);
+            }
+        }
+
+        if (stamina <= 0){
+            Speed = SpeedBase;
+        }
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+    public void ReducirStamina(int cantidad)
+    {
+        stamina -= cantidad;
+        if (stamina < 0) stamina = 0;
+        
+        barraStamina.ActualizarStamina(stamina);
+        Debug.Log("Stamina restante: " + stamina);
     }
 
     public void TakeDamage(float damage) {
