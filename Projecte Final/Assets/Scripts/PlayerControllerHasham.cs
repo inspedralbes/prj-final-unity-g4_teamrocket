c55471803175555e787946053e8cad8279776f02
a23matcasmar <a23matcasmar@inspedralbes.cat>
2025-05-07 09:41:31 +0200

    arreglo del mapa

--- Projecte Final/Assets/Scripts/PlayerControllerHasham.cs
+++ Projecte Final/Assets/Scripts/PlayerControllerHasham.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerHasham : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
